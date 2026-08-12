653b80549d8f51e29ed2f48cf307013155a50e3a
paradisecreate <radzivilchukedgar@gmail.com>
2026-08-12 19:22:38 +0000

    Use Free Tier eligible t3.micro instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -20,7 +20,7 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_instance" "devops_server" {
   ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
 
   iam_instance_profile = aws_iam_instance_profile.jenkins.name
 
