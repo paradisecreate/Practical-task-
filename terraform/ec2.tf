18325db3cc661f4cc0cc6c0503e38f404cd99fa9
paradisecreate <radzivilchukedgar@gmail.com>
2026-08-12 08:25:04 +0000

    Adding the EIP to the configuration

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,3 +6,26 @@ resource "aws_instance" "devops_server" {
     Name = "DevOps-team2"
   }
 }
+
+
+# Static public IP for the EC2 instance
+resource "aws_eip" "devops_server_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "DevOps-team2-eip"
+  }
+}
+
+
+# Attach Elastic IP to our EC2 instance
+resource "aws_eip_association" "devops_server_eip_assoc" {
+  instance_id   = aws_instance.devops_server.id
+  allocation_id = aws_eip.devops_server_eip.id
+}
+
+
+# Show the static IP after terraform apply
+output "devops_server_public_ip" {
+  value = aws_eip.devops_server_eip.public_ip
+}
